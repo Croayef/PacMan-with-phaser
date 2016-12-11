004df986913e7fdf746c6dda2d43f285a03aa1de
pogorzelski.a <pogorzelski.a@gmail.com>
2016-12-10 19:54:09 -0500

    msg

--- gameScript.js
+++ gameScript.js
@@ -1,4 +1,19 @@
-    var game = new Phaser.Game(640, 520, Phaser.CANVAS, 'game');
+  game = new Phaser.Game(640, 520, Phaser.CANVAS, 'game');
+
+  var stage = 1;
+
+var rand = function( min, max ){
+    min = parseInt( min, 10 );
+    max = parseInt( max, 10 );
+
+    if ( min > max ){
+        var tmp = min;
+        min = max;
+        max = tmp;
+    }
+
+    return Math.floor( Math.random() * ( max - min + 1 ) + min );
+}
 
     var PhaserGame = function (game) {
 
@@ -13,17 +28,23 @@
       this.alive = false;
       this.stateText = null;
       
-      this.stage = "Tile Layer 1";
+    
+      
       this.pacmanBegX = 1;
       this.pacmanBegY = 1;
-      this.redGhostBegX = 3;
-      this.redGhostBegY = 2;
+      
+      this.redGhostBegX = 4;
+      this.redGhostBegY = 4;
+      
       this.pinkGhostBegX = 0;
       this.pinkGhostBegY = 0;
+      
       this.orangeGhostBegX = 0;
       this.orangeGhostBegY = 0;
+      
       this.blueGhostBegX = 0;
       this.blueGhostBegY = 0;
+      
       this.purpleGhostBegX = 0;
       this.purpleGhostBegY = 0;
       
@@ -39,12 +60,17 @@
         this.turnSpeed = 150;
 
         this.marker = new Phaser.Point();
+      this.redMarker = new Phaser.Point();
+      this.redTurnPoint = new Phaser.Point();
         this.turnPoint = new Phaser.Point();
 
         this.directions = [ null, null, null, null, null ];
+      this.redDirections = [ null, null, null, null, null ];
         this.opposites = [ Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP ];
 
+      this.redCurrent = Phaser.NONE
         this.current = Phaser.UP;
+      this.redTurn = Phaser.NONE;
         this.turning = Phaser.NONE;
 
     };
@@ -62,8 +88,8 @@
             this.load.tilemap('map', 'assets/maze.json', null, Phaser.Tilemap.TILED_JSON);
             this.load.image('tiles', 'assets/tiles.png');
             this.load.image('dot', 'assets/dot.png');
-          this.load.spritesheet('pacman','assets/pacman-sprite.png',32,32);
-            this.load.spritesheet('sprites', 'assets/sprites.png',32,32);
+            this.load.spritesheet('pacman','assets/pacman.png',32,32);
+            this.load.spritesheet('redGhost', 'assets/redGhost.png',32,32);
 
         },
 
@@ -73,9 +99,24 @@
 
             this.map = this.add.tilemap('map');
             this.map.addTilesetImage('tiles', 'tiles');
-
-            this.layer = this.map.createLayer("Tile Layer 1");
           
+if( stage === 1) 
+       {this.pacmanBegX = 1;
+          this.pacmanBegY = 1;
+          this.redGhostBegX = 3;
+         this.redGhostBegY = 2;
+         this.layer = this.map.createLayer("Tile Layer 1");
+     }
+      else if(stage === 2){
+           this.pacmanBegX = 1;
+       this.pacmanBegY = 1;
+       this.redGhostBegX = 4;
+     this.redGhostBegY = 4;
+        this.layer = this.map.createLayer("Tile Layer 2");
+      }
+
+
+
           this.dots = this.add.physicsGroup();
    
           
@@ -91,45 +132,37 @@
             this.pacman = this.add.sprite((16+this.pacmanBegX*32),(16+this.pacmanBegY*32), 'pacman', 10);
             this.pacman.anchor.set(0.5);
           
-          this.redGhost = this.add.sprite((16+this.redGhostBegX*32),(16+this.redGhostBegY*32), 'sprites', 0)
+          this.redGhost = this.add.sprite((16+this.redGhostBegX*32),(16+this.redGhostBegY*32), 'redGhost', 0)
           this.redGhost.anchor.set(0.5);
           
           this.physics.arcade.enable(this.redGhost);
        
           
           //pacman HD
-            this.pacman.animations.add('munch', [0,1,2,3,2,1], 30, true);
+            this.pacman.animations.add('munch', [0,1], 10, true);
           
             this.physics.arcade.enable(this.pacman);
           
             this.cursors = this.input.keyboard.createCursorKeys();
 
            this.redGhost.animations.add('redHorizontalGhost',[0,1],8,true);
+          this.redGhost.animations.add('redUpGhost',[4,5],8,true);
+          this.redGhost.animations.add('redDownGhost',[2,3],8,true);
 
     
        
            this.pacman.play('munch');
            this.redGhost.play('redHorizontalGhost');
             //  Text
-        this.stateText = this.add.text(this.world.centerX,this.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
+        this.stateText = this.add.text(this.world.centerX,this.world.centerY,' ', { font: '74px Arial', fill: '#fff' });
         this.stateText.anchor.setTo(0.5, 0.5);
         this.stateText.visible = false;
-          
-          
-          
+
           //  The score
         this.scoreString = 'Score : ';
-      
         this.scoreText = this.add.text(10, 480, this.scoreString + this.score, { font: '34px Arial', fill: '#fff' });
           
-        
-      
-    
-          
           this.move(Phaser.DOWN);
-
-          
-          
           
         },
 
@@ -156,10 +189,35 @@
                 //  This forces them to hold the key down to turn the corner
                 this.turning = Phaser.NONE;
             }
+   
 
         },
+      checkRed: function () {
 
-        checkDirection: function (turnTo) {
+            if ( this.redTurn === Phaser.LEFT && this.redCurrent !== Phaser.LEFT)
+            {
+                this.checkDirection(Phaser.LEFT);
+            }
+            else if (this.redTurn === Phaser.LEFT && this.redCurrent !== Phaser.RIGHT)
+            {
+                this.checkDirection(Phaser.RIGHT);
+            }
+            else if (this.redTurn === Phaser.LEFT && this.redCurrent !== Phaser.UP)
+            {
+                this.checkDirection(Phaser.UP);
+            }
+            else if (this.redTurn === Phaser.LEFT && this.redCurrent !== Phaser.DOWN)
+            {
+                this.checkDirection(Phaser.DOWN);
+            }
+            else
+            {
+                this.redTurn = Phaser.NONE;
+            }
+
+        },
+      
+      checkDirection: function (turnTo) {
 
             if (this.turning === turnTo || this.directions[turnTo] === null || this.directions[turnTo].index !== this.safetile)
             {
@@ -182,7 +240,56 @@
             }
 
         },
+      
+      
+        checkRedDirection: function (turnTo) {
 
+            if (this.redTurn === turnTo || this.redDirections[turnTo] === null || this.redDirections[turnTo].index !== this.safetile)
+            {
+                //  Invalid direction if they're already set to turn that way
+                //  Or there is no tile there, or the tile isn't index a floor tile
+                return;
+            }
+
+            //  Check if they want to turn around and can
+            if (this.redCurrent === this.opposites[turnTo])
+            {
+                this.redMove(turnTo);
+            }
+            else
+            {
+                this.redTurn = turnTo;
+
+                this.redTurnPoint.x = (this.redMarker.x * this.gridsize) + (this.gridsize / 2);
+                this.redTurnPoint.y = (this.redMarker.y * this.gridsize) + (this.gridsize / 2);
+            }
+
+        },
+
+      redGhostTurn: function(){
+        
+          var cx = Math.floor(this.redGhost.x);
+           var cy = Math.floor(this.redGhost.y);
+
+            //  This needs a threshold, because at high speeds you can't turn because the coordinates skip past
+            if (!this.math.fuzzyEqual(cx, this.redTurnPoint.x, this.redTurnPoint) || !this.math.fuzzyEqual(cy, this.redTurnPoint.y, this.threshold))
+            {
+                return false;
+            }
+
+            this.redGhost.x = this.redTurnPoint.x;
+            this.redGhost.y = this.redTurnPoint.y;
+
+           this.redGhost.body.reset(this.redTurnPoint.x, this.redTurnPoint.y);
+          
+            this.redMove(this.redTurn);
+
+            this.redTurn = Phaser.NONE;
+
+            return true;
+        
+      },
+      
         turn: function () {
 
             var cx = Math.floor(this.pacman.x);
@@ -207,6 +314,46 @@
 
         },
 
+      redMove: function(direction){
+        
+        var speed = this.speed;
+
+            if (direction === Phaser.LEFT || direction === Phaser.UP)
+            {
+                speed = -speed;
+            }
+
+            if (direction === Phaser.LEFT || direction === Phaser.RIGHT)
+            {
+                this.redGhost.body.velocity.x = speed;
+            }
+            else
+            {
+                this.redGhost.body.velocity.y = speed;
+            }
+
+            this.redGhost.scale.x = 1;
+            this.redGhost.angle = 0;
+          
+           
+        
+          if (direction === Phaser.LEFT)
+            {
+                this.redGhost.scale.x = -1;
+            }
+            else if (direction === Phaser.UP)
+            {
+                this.redGhost.play('redUpGhost');
+            }
+            else if (direction === Phaser.DOWN)
+            {
+                this.redGhost.play('redDownGhost');
+            }
+
+            this.redCurrent = direction;
+        
+      },
+      
         move: function (direction) {
 
             var speed = this.speed;
@@ -249,119 +396,100 @@
 
         },
       
+     
       eatDot: function (pacman, dot) {
 
             dot.kill();
         this.score++;
         this.scoreText.text = this.scoreString + this.score;
-           // if (this.dots.total === 0)
-          //  {
-          //      this.dots.callAll('revive');
-          //  }
+            if (this.dots.total === 0)
+            {
+                if(stage < 2)
+                  stage ++;
+                this.pacman.kill();
+                this.redGhost.kill();
+                this.dots.callAll('kill');
+                this.stateText.text=" YOU WON \n Click to try next level";
+              this.stateText.visible = true;
+              this.input.onTap.addOnce(this.restart,this);
+            }
 
         },
       
         die: function () 
       {
-          this.score = -1;
         
+        this.score = -1;
         this.pacman.kill();
         this.redGhost.kill();
         this.dots.callAll('kill');
         this.stateText.text=" GAME OVER \n Click to restart";
         this.stateText.visible = true;
-
-        //the "click to restart" handler
         this.input.onTap.addOnce(this.restart,this);
-        
         },
 
         update: function () {
           
-        
-           
+         
+          var rnd = rand(0,100);
+          if(rnd < 25){
+            this.redTurn = Phaser.LEFT;
+          }
+          else if(rnd < 50){
+            this.redTurn = Phaser.RIGHT;
+          }
+          else if(rnd < 75){
+            this.redTurn = Phaser.UP;
+          }
+          else{
+            this.redTurn = Phaser.DOWN;
+          }
+          
+          
           this.physics.arcade.collide(this.pacman, this.layer);
           this.physics.arcade.overlap(this.pacman, this.redGhost, this.die, null, this);
           this.physics.arcade.overlap(this.pacman, this.dots, this.eatDot, null, this);
+          this.physics.arcade.collide(this.redGhost, this.layer);
           
-
+          this.marker.x = this.math.snapToFloor(Math.floor(this.pacman.x), this.gridsize) / this.gridsize;
+          this.marker.y = this.math.snapToFloor(Math.floor(this.pacman.y), this.gridsize) / this.gridsize;
           
-           this.marker.x = this.math.snapToFloor(Math.floor(this.pacman.x), this.gridsize) / this.gridsize;
-           this.marker.y = this.math.snapToFloor(Math.floor(this.pacman.y), this.gridsize) / this.gridsize;
+          this.redMarker.x = this.math.snapToFloor(Math.floor(this.pacman.x), this.gridsize) / this.gridsize;
+          this.redMarker.y = this.math.snapToFloor(Math.floor(this.pacman.y), this.gridsize) / this.gridsize;
           
-            //  Update our grid sensors
-            this.directions[1] = this.map.getTileLeft(this.layer.index, this.marker.x, this.marker.y);
-            this.directions[2] = this.map.getTileRight(this.layer.index, this.marker.x, this.marker.y);
-            this.directions[3] = this.map.getTileAbove(this.layer.index, this.marker.x, this.marker.y);
-            this.directions[4] = this.map.getTileBelow(this.layer.index, this.marker.x, this.marker.y);
-
-            this.checkKeys();
-
-            if (this.turning !== Phaser.NONE)
+          //  Update our grid sensors
+          this.directions[1] = this.map.getTileLeft(this.layer.index, this.marker.x, this.marker.y);
+          this.directions[2] = this.map.getTileRight(this.layer.index, this.marker.x, this.marker.y);
+          this.directions[3] = this.map.getTileAbove(this.layer.index, this.marker.x, this.marker.y);
+          this.directions[4] = this.map.getTileBelow(this.layer.index, this.marker.x, this.marker.y);
+          
+          this.redDirections[1] = this.map.getTileLeft(this.layer.index, this.redMarker.x, this.redMarker.y);
+          this.redDirections[2] = this.map.getTileRight(this.layer.index, this.redMarker.x, this.redMarker.y);
+          this.redDirections[3] = this.map.getTileAbove(this.layer.index, this.redMarker.x, this.redMarker.y);
+          this.redDirections[4] = this.map.getTileBelow(this.layer.index, this.redMarker.x, this.redMarker.y);
+          
+          this.checkKeys();
+          
+          if (this.turning !== Phaser.NONE)
             {
                 this.turn();
             }
+          
+          if(this.redTurn !== Phaser.NONE)
+          {
+            this.redGhostTurn();
+          }
+          
+            
 
-        },
-      restartPacmanPosition : function(){
-        this.pacman.x = 48;
-        this.pacman.y = 48;
-        
-      },
-      restartGhostsPositions : function(){
-        this.redGhost.body.velocity.x = 0;
-        this.redGhost.body.velocity.y = 0;
-        this.redGhost.x = (48+2*32)
-        this.redGhost.y = (48+1*32);
-      },
-        restart : function() {
-
-            //  A new level starts
-          
-          this.dots.callAll('revive');
-          this.restartPacmanPosition();
-          this.restartGhostsPositions();
-          //revives the player
-         this.pacman.revive();
-         this.redGhost.revive();
-         this.scoreText.text = this.scoreString + this.score;
-            //hides the text
-          this.stateText.visible = false;
-          
-          this.move(Phaser.DOWN);
-          
         },
       
-        render: function () {
-
-            //  Un-comment this to see the debug drawing
-/*
-            for (var t = 1; t < 5; t++)
-            {
-                if (this.directions[t] === null)
-                {
-                    continue;
-                }
-
-                var color = 'rgba(0,255,0,0.3)';
-
-                if (this.directions[t].index !== this.safetile)
-                {
-                    color = 'rgba(255,0,0,0.3)';
-                }
-
-                if (t === this.current)
-                {
-                    color = 'rgba(255,255,255,0.3)';
-                }
-
-                this.game.debug.geom(new Phaser.Rectangle(this.directions[t].worldX, this.directions[t].worldY, 32, 32), color, true);
-            }
-
-           // this.game.debug.geom(this.turnPoint, '#ffff00');
-*/
+        restart : function() {
+          this.state.restart();
         }
-
+      
     };
 
-    game.state.add('Game', PhaserGame, true);
+
+game.state.add('Game', PhaserGame);
+game.state.start('Game');
